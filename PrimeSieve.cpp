1ba16f4710048af97c0fda189f992c58e1bd731f
Code-Agon <41378725+Code-Agon@users.noreply.github.com>
2020-09-18 11:28:10 +0530

    Update PrimeSieve.cpp

--- PrimeSieve.cpp
+++ PrimeSieve.cpp
@@ -3,20 +3,11 @@ bool prime[1000001];
 vector<int> v;
 void SieveOfEratosthenes(int n) 
 { 
-    // Create a boolean array "prime[0..n]" and initialize 
-    // all entries it as true. A value in prime[i] will 
-    // finally be false if i is Not a prime, else true. 
     memset(prime, true, sizeof(prime)); 
-  
     for (int p=2; p*p<=n; p++) 
     { 
-        // If prime[p] is not changed, then it is a prime 
         if (prime[p] == true) 
         { 
-            // Update all multiples of p greater than or  
-            // equal to the square of it 
-            // numbers which are multiple of p and are 
-            // less than p^2 are already been marked.  
             for (int i=p*p; i<=n; i += p) 
                 prime[i] = false; 
         } 
