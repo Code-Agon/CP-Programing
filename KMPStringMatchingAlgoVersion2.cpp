6c7ed88e7c79a7a2d28ce2ac68aea744f5d69776
Code-Agon <41378725+Code-Agon@users.noreply.github.com>
2020-10-10 18:28:45 +0530

    Update KMPStringMatchingAlgoVersion2.cpp

--- KMPStringMatchingAlgoVersion2.cpp
+++ KMPStringMatchingAlgoVersion2.cpp
@@ -1,7 +1,23 @@
+/**
+https://cses.fi/problemset/task/1753/
+Given a string and a pattern, your task is to count the number of positions where the pattern occurs in the string.
+Input
+The first input line has a string of length n, and the second input line has a pattern of length m. Both of them consist of characters a–z.
+Output
+Print one integer: the number of occurrences.
+Constraints
+1≤n,m≤106
+Example
+Input:
+saippuakauppias
+pp
+Output:
+2
+**/
+
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
    string str,pat;
