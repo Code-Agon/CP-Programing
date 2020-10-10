62dda2726e2e501f26d0cf8dcbf08eb94c74f0eb
Code-Agon <41378725+Code-Agon@users.noreply.github.com>
2020-10-10 11:32:07 +0530

    Update AreaOftheDotsThatcanHaveInAllFourSidesinA2DMatrix.pp

--- PracticedProblems/AreaOftheDotsThatcanHaveInAllFourSidesinA2DMatrix.pp
+++ PracticedProblems/AreaOftheDotsThatcanHaveInAllFourSidesinA2DMatrix.pp
@@ -1,4 +1,5 @@
 /**
+https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/algorithm/areas-0475fb6e/
 You are given a 2D grid. A '#' represents an obstacle and a '.' represents free space. 
 You need to find the areas of the disconnected components. The cells (i+1, j), (i, j+1), (i-1, j), (i, j-1) are the adjacent to the cell (i, j).
 
