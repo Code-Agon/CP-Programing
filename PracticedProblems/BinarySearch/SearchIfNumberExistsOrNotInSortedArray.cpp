5c79af772e5ab21805259ea2df65b02746186860
Code-Agon <41378725+Code-Agon@users.noreply.github.com>
2020-10-11 19:19:14 +0530

    Update SearchIfNumberExistsOrNotInSortedArray.cpp

--- PracticedProblems/BinarySearch/SearchIfNumberExistsOrNotInSortedArray.cpp
+++ PracticedProblems/BinarySearch/SearchIfNumberExistsOrNotInSortedArray.cpp
@@ -4,69 +4,40 @@ Implement a binary search algorithm.
 **/
 
 #include <iostream>
-#include <bits/stdc++.h>
+ 
 using namespace std;
-int
-search_r (int arr[], int n, int k)
+bool
+search (int arr[], int n, int k)
 {
-  int ma = -1;
   int lo = 0, hig = n - 1;
   while (lo <= hig)
     {
       int mid = lo + (hig - lo) / 2;
-      if (arr[mid] <= k)
-	{
- 
-	  lo = mid + 1;
-	  ma = mid;
-	}
+      if (arr[mid] == k)
+	return true;
+      if (arr[mid] < k)
+	lo = mid + 1;
       else
 	hig = mid - 1;
     }
-  return ma + 1;;
-}
-int
-search (int arr[], int n, int k)
-{
-  int ma = -1;
-  int lo = 0, hig = n - 1;
-  while (lo <= hig)
-    {
-      int mid = lo + (hig - lo) / 2;
-      if (arr[mid] <k)
-	{
- 
-	  lo = mid + 1;
-	  
-	}
-      else{
-          
-	hig = mid - 1;
-	ma=mid;
-      }
-    }
-  return ma + 1;;
+  return false;
 }
  
 int
 main ()
 {
   int t, k;
-  cin >> t;
+  cin >> t >> k;
   int arr[t];
   for (int i = 0; i < t; i++)
     cin >> arr[i];
-    cin>>k;
-    sort(arr,arr+t);
   for (int i = 0; i < k; i++)
     {
-      int l,r;
-      cin >> l>>r;
-      int les = search_r(arr, t, r);
-      int gret = search(arr,t,l);
-      if(les==0 || gret==0) cout<<0<<" ";
-      else cout << les-gret+1 << " ";
- 
+      int g;
+      cin >> g;
+      bool bo = search (arr, t, g);
+      if(bo) cout<<"YES\n";
+      else cout<<"NO\n";
     }
  
   return 0;
