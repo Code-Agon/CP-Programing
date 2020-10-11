d8076d5b86a8d91facd245ba4f41b3b3a36b721a
Code-Agon <41378725+Code-Agon@users.noreply.github.com>
2020-10-11 19:25:43 +0530

    Create NoOfValuesInbetweenLandR.cpp

---
+++ PracticedProblems/BinarySearch/NoOfValuesInbetweenLandR.cpp
@@ -0,0 +1,74 @@
+/**
+https://codeforces.com/edu/course/2/lesson/6/1/practice/contest/283911/problem/D
+You are given an array a of n integers a1,a2,…,an.
+Your task is to response to the queries like "How many numbers' values are between l and r?".
+**/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int
+search_r (int arr[], int n, int k)
+{
+  int ma = -1;
+  int lo = 0, hig = n - 1;
+  while (lo <= hig)
+    {
+      int mid = lo + (hig - lo) / 2;
+      if (arr[mid] <= k)
+	{
+ 
+	  lo = mid + 1;
+	  ma = mid;
+	}
+      else
+	hig = mid - 1;
+    }
+  return ma + 1;;
+}
+int
+search (int arr[], int n, int k)
+{
+  int ma = -1;
+  int lo = 0, hig = n - 1;
+  while (lo <= hig)
+    {
+      int mid = lo + (hig - lo) / 2;
+      if (arr[mid] <k)
+	{
+ 
+	  lo = mid + 1;
+	  
+	}
+      else{
+          
+	hig = mid - 1;
+	ma=mid;
+      }
+    }
+  return ma + 1;;
+}
+ 
+int
+main ()
+{
+  int t, k;
+  cin >> t;
+  int arr[t];
+  for (int i = 0; i < t; i++)
+    cin >> arr[i];
+    cin>>k;
+    sort(arr,arr+t);
+  for (int i = 0; i < k; i++)
+    {
+      int l,r;
+      cin >> l>>r;
+      int les = search_r(arr, t, r);
+      int gret = search(arr,t,l);
+      if(les==0 || gret==0) cout<<0<<" ";
+      else cout << les-gret+1 << " ";
+ 
+    }
+ 
+  return 0;
+}
